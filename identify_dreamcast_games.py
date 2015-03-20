ddd32916f0c2dce6eea003f123e8e0327c88fdd1
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-03-20 07:10:19 -0700

    Made many functions private.

--- identify_dreamcast_games.py
+++ identify_dreamcast_games.py
@@ -35,7 +35,7 @@ BUFFER_SIZE = 1024 * 1024 * 10
 IS_PY2 = sys.version_info[0] == 2
 
 
-def strip_comments(data):
+def _strip_comments(data):
 	lines = data.split(b"\r\n")
 	data = []
 	for line in lines:
@@ -46,16 +46,16 @@ def strip_comments(data):
 			
 
 with open('db_dreamcast_unofficial.json', 'rb') as f:
-	unofficial_db = json.loads(strip_comments(f.read()).decode('utf8'))
+	unofficial_db = json.loads(_strip_comments(f.read()).decode('utf8'))
 
 with open('db_dreamcast_official_us.json', 'rb') as f:
-	official_us_db = json.loads(strip_comments(f.read()).decode('utf8'))
+	official_us_db = json.loads(_strip_comments(f.read()).decode('utf8'))
 
 with open('db_dreamcast_official_jp.json', 'rb') as f:
-	official_jp_db = json.loads(strip_comments(f.read()).decode('utf8'))
+	official_jp_db = json.loads(_strip_comments(f.read()).decode('utf8'))
 
 with open('db_dreamcast_official_eu.json', 'rb') as f:
-	official_eu_db = json.loads(strip_comments(f.read()).decode('utf8'))
+	official_eu_db = json.loads(_strip_comments(f.read()).decode('utf8'))
 
 
 # Convert the keys from strings to bytes
@@ -81,13 +81,13 @@ for db in dbs:
 			db[bytes(key, 'utf-8')] = val
 
 
-def read_blob_at(file, start_address, size):
+def _read_blob_at(file, start_address, size):
 	file.seek(start_address)
 	blob = file.read(size)
 	return blob
 
 # FIXME: Look up these games and make sure it works
-def fix_games_with_same_serial_number(f, title, serial_number):
+def _fix_games_with_same_serial_number(f, title, serial_number):
 	'''
 	if serial_number == 'T8116D  05':
 		EU ECW Hardcore Revolution
@@ -124,71 +124,60 @@ def fix_games_with_same_serial_number(f, title, serial_number):
 
 	return (title, serial_number)
 
-def fix_games_that_are_mislabelled(f, title, serial_number):
+def _fix_games_that_are_mislabelled(f, title, serial_number):
 	if serial_number == b"T1402N": # Mr. Driller
-		if read_blob_at(f, 0x159208, 12) == b"DYNAMITE COP":
+		if _read_blob_at(f, 0x159208, 12) == b"DYNAMITE COP":
 			return (u"Dynamite Cop!", "MK-51013")
 	elif serial_number == b"MK-51035": # Crazy Taxi
-		if read_blob_at(f, 0x1617E654, 9) == b"Half-Life":
+		if _read_blob_at(f, 0x1617E654, 9) == b"Half-Life":
 			return (u"Half-Life", "T0000M")
-		elif read_blob_at(f, 0x1EA78B5, 10) == b"Shadow Man":
+		elif _read_blob_at(f, 0x1EA78B5, 10) == b"Shadow Man":
 			return (u"Shadow Man", "T0000M")
 	elif serial_number == b"T43903M": # Culdcept II
-		if read_blob_at(f, 0x264E1E5D, 10) == b"CHAOSFIELD":
+		if _read_blob_at(f, 0x264E1E5D, 10) == b"CHAOSFIELD":
 			return (u"Chaos Field", "T47801M")
 	elif serial_number == b"T0000M": # Unnamed
-		if read_blob_at(f, 0x557CAB0, 13) == b"BALL BREAKERS":
+		if _read_blob_at(f, 0x557CAB0, 13) == b"BALL BREAKERS":
 			return (u"Ball Breakers", "T0000M")
-		elif read_blob_at(f, 0x4BD5EE5, 6) == b"TOEJAM":
+		elif _read_blob_at(f, 0x4BD5EE5, 6) == b"TOEJAM":
 			return (u"ToeJam and Earl 3", "T0000M")
 	elif serial_number == b"T0000": # Unnamed
-		if read_blob_at(f, 0x162E20, 15) == b"Art of Fighting":
+		if _read_blob_at(f, 0x162E20, 15) == b"Art of Fighting":
 			return (u"Art of Fighting", "T0000")
-		elif read_blob_at(f, 0x29E898B0, 17) == b"Art of Fighting 2":
+		elif _read_blob_at(f, 0x29E898B0, 17) == b"Art of Fighting 2":
 			return (u"Art of Fighting 2", "T0000")
-		elif read_blob_at(f, 0x26D5BCA4, 17) == b"Art of Fighting 3":
+		elif _read_blob_at(f, 0x26D5BCA4, 17) == b"Art of Fighting 3":
 			return (u"Art of Fighting 3", "T0000")
-		elif read_blob_at(f, 0x295301F0, 5) == b"Redux":
+		elif _read_blob_at(f, 0x295301F0, 5) == b"Redux":
 			return (u"Redux: Dark Matters", "T0000")
 	elif serial_number == b"MK-51025": # NHL 2K1
-		if read_blob_at(f, 0x410CA8, 14) == b"READY 2 RUMBLE":
+		if _read_blob_at(f, 0x410CA8, 14) == b"READY 2 RUMBLE":
 			return (u"Ready 2 Rumble Boxing", "T0000")
 	elif serial_number == b"T36804N": # Walt Disney World Quest: Magical Racing Tour
-		if read_blob_at(f, 0x245884, 6) == b"MakenX":
+		if _read_blob_at(f, 0x245884, 6) == b"MakenX":
 			return (u"Maken X", "MK-51050")
 	elif serial_number == b"RDC-0117": # The king of Fighters '96 Collection (NEO4ALL RC4)
-		if read_blob_at(f, 0x159208, 16) == b"BOMBERMAN ONLINE":
+		if _read_blob_at(f, 0x159208, 16) == b"BOMBERMAN ONLINE":
 			return (u"Bomberman Online", "RDC-0120")
 	elif serial_number == b"RDC-0140": # Dead or Alive 2
-		if read_blob_at(f, 0x15639268, 13) == b"CHUCHU ROCKET":
+		if _read_blob_at(f, 0x15639268, 13) == b"CHUCHU ROCKET":
 			return (u"ChuChu Rocket!", "RDC-0139")
 	elif serial_number == b"T19724M": # Pizzicato Polka: Suisei Genya
-		if read_blob_at(f, 0x3CA16B8, 7) == b"DAYTONA":
+		if _read_blob_at(f, 0x3CA16B8, 7) == b"DAYTONA":
 			return (u"Daytona USA", "MK-51037")
 	elif serial_number == b"MK-51049": # ChuChu Rocket!
-		if read_blob_at(f, 0xC913DDC, 13) == b"HYDRO THUNDER":
+		if _read_blob_at(f, 0xC913DDC, 13) == b"HYDRO THUNDER":
 			return (u"Hydro Thunder", "T9702N")
-		elif read_blob_at(f, 0x2D096802, 17) == b"MARVEL VS. CAPCOM":
+		elif _read_blob_at(f, 0x2D096802, 17) == b"MARVEL VS. CAPCOM":
 			return (u"Marvel vs. Capcom 2", "T1212N")
 	elif serial_number == b"T44304N": # Sports Jam
-		if read_blob_at(f, 0x157FA8, 9) == b"OUTRIGGER":
+		if _read_blob_at(f, 0x157FA8, 9) == b"OUTRIGGER":
 			return (u"OutTrigger: International Counter Terrorism Special Force", "MK-51102")
 
 	return (title, serial_number)
 
 
-def is_dreamcast_file(game_file):
-	# Skip if not file
-	if not os.path.isfile(game_file):
-		return False
-
-	# Skip if not a usable file
-	if not os.path.splitext(game_file)[1].lower() in ['.cdi', '.gdi', '.iso', '.mdf']:
-		return False
-
-	return True
-
-def locate_string_in_file(f, file_size, string_to_find):
+def _locate_string_in_file(f, file_size, string_to_find):
 	string_length = len(string_to_find)
 	f.seek(0)
 	while True:
@@ -218,7 +207,7 @@ def locate_string_in_file(f, file_size, string_to_find):
 
 	return -1
 
-def get_track_01_from_gdi_file(file_name):
+def _get_track_01_from_gdi_file(file_name):
 	path = os.path.dirname(file_name)
 	if IS_PY2:
 		path = bytes(path)
@@ -231,6 +220,16 @@ def get_track_01_from_gdi_file(file_name):
 		track_01_file = os.path.join(path, track_01_file)
 		return track_01_file
 
+def is_dreamcast_file(game_file):
+	# Skip if not file
+	if not os.path.isfile(game_file):
+		return False
+
+	# Skip if not a usable file
+	if not os.path.splitext(game_file)[1].lower() in ['.cdi', '.gdi', '.iso', '.mdf']:
+		return False
+
+	return True
 
 def get_dreamcast_game_info(game_file):
 	# Get the full file name
@@ -238,7 +237,7 @@ def get_dreamcast_game_info(game_file):
 
 	# If it's a GDI file read track 01
 	if os.path.splitext(full_entry)[1].lower() == '.gdi':
-		full_entry = get_track_01_from_gdi_file(full_entry)
+		full_entry = _get_track_01_from_gdi_file(full_entry)
 
 	# Open the game file
 	f = open(full_entry, 'rb', buffering=BUFFER_SIZE)
@@ -246,7 +245,7 @@ def get_dreamcast_game_info(game_file):
 
 	# Get the location of the header
 	header_text = b"SEGA SEGAKATANA SEGA ENTERPRISES"
-	index = locate_string_in_file(f, file_size, header_text)
+	index = _locate_string_in_file(f, file_size, header_text)
 	
 	# Throw if index not found
 	if index == -1:
@@ -283,10 +282,10 @@ def get_dreamcast_game_info(game_file):
 		title = official_jp_db[serial_number]
 
 	# Check for games with the same serial number
-	title, serial_number = fix_games_with_same_serial_number(f, title, serial_number)
+	title, serial_number = _fix_games_with_same_serial_number(f, title, serial_number)
 
 	# Check for mislabelled releases
-	title, serial_number = fix_games_that_are_mislabelled(f, title, serial_number)
+	title, serial_number = _fix_games_that_are_mislabelled(f, title, serial_number)
 
 	f.close()
 
